0b993c59e8ff5560587b56b682e672e1d372fcc6
Victor Adeyemo <billionaireteacher@gmail.com>
2023-01-26 06:45:02 -0800

    Prints strings to stdout without handling format speciification

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ *_printf - A function that works as the default printf program in c
+ *@format: the character supplied
+ * Return: void.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int n_displayed = 0;
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		_putchar(format[i]);
+
+		i++;
+		n_displayed += 1;
+	}
+
+	va_end(args);
+	return (n_displayed);
+}
---
+++ test/main.c
@@ -0,0 +1,15 @@
+#include "../main.h"
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * main - This is the main C function
+ * Return: Always return 0.
+ *
+ */
+
+int main(void){
+	 _printf("Hello World\n");
+
+	 return (0);
+}
